038491dfa33320c30289d2bd89c6e1cfd5110ef6
= <=>
2022-08-24 22:04:46 -0500

    Agregando cambios nuevos

--- src/main/java/co/gov/misiontic/cartera/ingresoegreso/entity/TipoDocumento.java
+++ src/main/java/co/gov/misiontic/cartera/ingresoegreso/entity/TipoDocumento.java
@@ -2,5 +2,8 @@ package co.gov.misiontic.cartera.ingresoegreso.entity;
 
 public class TipoDocumento {
     private long idTipoDocumento;
+    private String descripcion;
+    private String siglas;
+    private boolean estado;
 
 }
